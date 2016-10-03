e653a7a2be7fc38b0b5493e649480d1faa633dcd
HaunsTM <hans.baldetorp@gmail.com>
2016-10-03 15:37:36 +0200

    Experiment med logoutfunktion

--- advent-calendar/angularJSApp/components/logout/logoutController.js
+++ advent-calendar/angularJSApp/components/logout/logoutController.js
@@ -1,3 +1,24 @@
-﻿adventCalendarApp.controller('logoutController', function ($scope) {
-    $scope.Message = "This is logoutController page";
-})
+﻿adventCalendarApp.controller('logoutController', ['$scope', '$location', 'logoutFactory', 'sessionService', function ($scope, $location, logoutFactory, sessionService) {
+
+        $scope.Message = "This is logoutController page";
+
+        $scope.loginForm = {
+            username: undefined,
+            password: undefined,
+            errorMessage: undefined
+        };
+
+        $scope.logout = function () {
+            debugger;
+            logoutFactory()
+                .then(function (response) {
+                    debugger;
+                    sessionService.setToken(response.access_token);
+                    $location.path('/');
+                }, function (response) {
+                    debugger;
+                    $scope.loginForm.errorMessage = response.error_description;
+                });
+        }
+    }
+]);
---
+++ advent-calendar/angularJSApp/shared/factories/logoutFactory.js
@@ -0,0 +1,20 @@
+﻿adventCalendarApp.factory('logoutFactory', ['$http', '$q', 'sessionService', function ($http, $q, sessionService) {
+
+    return function () {
+        var result = $q.defer();
+        $http({
+            method: 'POST',
+            url: sessionService.apiUrl + '/Account/Logout'
+        })
+        .success(function (response) {
+            result.resolve(response);
+        })
+        .error(function (response) {
+            result.reject(response);
+        });
+
+        return result.promise;
+    }
+
+
+}]);
